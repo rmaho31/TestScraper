b1dfdf958cd8da8342d4aaccd5c37dd5c9a41895
Robert Mahoney <robert.w.mahoney@gmail.com>
2018-09-02 16:10:06 -0400

    first

---
+++ ScrapingData.py
@@ -0,0 +1,90 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+from tabulate import tabulate
+
+# launch url
+url = "http://buyvia.cnxpartner.com/"
+
+# create a new Chrome session
+driver = webdriver.Chrome(executable_path='F:\Python_Workspace\TestScraper\chromedriver.exe')
+driver.implicitly_wait(30)
+driver.get(url)
+
+# After opening the url clicks the link
+python_link = driver.find_element_by_link_text('Appliances')
+python_link.click()  # click link
+
+# give pagesource to beautifulsoup
+pagesrc1 = BeautifulSoup(driver.page_source, 'lxml')
+
+# setup dataFrame
+df = pd.DataFrame([],
+                  columns=["VendorID", "PartNumber", "Name", "Price", "Description", "PhotoPath"])
+list1 = []
+# selects the elements that have the product name information contained in the div class title
+for vendor in pagesrc1.find_all('div', class_=re.compile("^merchantLogo")):
+    img = vendor.find('img', alt=True)
+
+    list1.append(str(img['alt']))\
+
+    # end loop block
+
+# loop has completed add list to dataframe
+df['VendorID'] = list1
+
+list1 = []
+# selects the elements that have the partNumber information just made up from the image path
+for partNumber in pagesrc1.find_all('div', class_=re.compile("^col-xs-3 col-md-2 listProdImg")):
+    img = partNumber.find('img')
+
+    list1.append(re.sub(r";", "", re.search(r"\d+?;", str(img['src'])).group(0)))
+
+    # end loop block
+
+# loop has completed add list to dataframe
+df['PartNumber'] = list1
+
+list1 = []
+# selects the elements that have the product name information contained in the div class title
+for title in pagesrc1.find_all('div', class_=re.compile("^title")):
+
+    list1.append(str(title.string))
+
+    # end loop block
+
+# loop has completed add list to dataframe
+df['Name'] = list1
+
+list1 = []
+# selects the price information from the div class and adds to the list
+for price in pagesrc1.find_all('div', id=re.compile("^offerPrice")):
+
+    list1.append(re.sub(r"[$,]", "", str(price.string)))
+
+    # end loop block
+
+# loop has completed add list to dataframe
+df['Price'] = list1
+
+list1 = []
+# selects the description information from the page and adds it to the list
+for description in pagesrc1.find_all('p', class_=re.compile("^desc")):
+
+    list1.append(str(description.string))
+
+    # end loop block
+
+# loop has completed add list to dataframe
+df['Description'] = list1
+
+# goes back one page
+driver.execute_script("window.history.go(-1)")
+
+# end the Selenium browser session
+driver.quit()
+
+# view dataframe
+print(tabulate(df))
